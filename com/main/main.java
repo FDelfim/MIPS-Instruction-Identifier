c636c80d5e3707970eb324769bd3a0402344938f
Felipe Delfim Machado <38048491+FDelfim@users.noreply.github.com>
2023-01-17 20:15:43 -0300

    regex

--- com/main/main.java
+++ com/main/main.java
@@ -3,34 +3,44 @@ package com.main;
 import java.util.Scanner;
 import java.io.File;
 
-public class main{
+class main {
+    public static void main(String args[]) {
 
-    public static void main(String[] args){
+        String reg = "\\$(t||s)[0-9]";
+        String soma = "add " + reg + "," + reg + "," + reg;
+        String sub = "sub " + reg + "," + reg + "," + reg;
+        String sw = "sw " + reg + "," + "[0-10000]" + "(" + reg + ")";
+        String lw = "lw " + reg + "," + "[0-10000]" + "(" + reg + ")";
+        String memory = "memory\\[" + "[0-9]" + "\\]";
 
-        try{
+        try {
             File file = new File("arquivo.txt");
-            Scanner sc = new Scanner(file); 
+            Scanner sc = new Scanner(file);
 
             String texto[];
 
-            while(sc.hasNextLine())
-            {
+            while (sc.hasNextLine()) {
                 texto = sc.nextLine().split("\n");
-                for(int i = 0; i < texto.length; i++)
-                {
-                    String op[] = texto[i].split(" ");
-                    if(op.length == 1){
-                        operacao operacao = new operacao(op[0]);
-                        System.out.println(operacao.operacaoValida());
+                for (int i = 0; i < texto.length; i++) {
+                    if (texto[i].matches(soma)) {
+                        System.out.println(texto[i] + " Operação de soma\n");
                     }
-                    else{
-                        operacao operacao = new operacao(op[0], op[1]);
-                        System.out.println(operacao.operacaoValida());
+                    if (texto[i].matches(sub)) {
+                        System.out.println(texto[i] + " Operação de subtração\n");
+                    }
+                    if (texto[i].matches(sw)) {
+                        System.out.println(texto[i] + " Operação SW\n");
+                    }
+                    if (texto[i].matches(lw)) {
+                        System.out.println(texto[i] + " Operação LW\n");
+                    }
+                    if (texto[i].matches(memory)) {
+                        System.out.println(texto[i] + " Operação memory\n");
                     }
                 }
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
     }
